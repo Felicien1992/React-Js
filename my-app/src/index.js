7195217cb391fb35f8e7aec1da277ffc578962d3
Felicien1992 <felicienmukesharugamba@gmail.com>
2023-12-04 12:10:43 +0200

    add images folder & many things

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,80 @@
-import React from "react"
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-function Greetings () {
-    return <h2>My First Component</h2>
+
+const firstBook = {
+  img: './images/imag1.jpg',
+  author: 'Felicien',
+  title: 'Best Book',
+}
+const secondBook = {
+  img: './images/imag3.jpg',
+  author: 'Blaisby',
+  title: 'Funny Book',
+}
+const thirthBook = {
+  img: './images/imag6.jpg',
+  author: 'Synforien',
+  title: 'Commedy Book',
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const Book = (props) => {
+//   return (
+//     <article className="book">
+//       <img src={props.img} />
+//       <h2>{props.author}</h2>
+//       <h4>{props.title}</h4>
+//     </article>
+//   )
+// }
 
-root.render(<Greetings/>)
+
+// function BookList() {
+//   return (
+//     <section className="booklist">
+//       <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
+//       <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+//       <Book author={thirthBook.author} title={thirthBook.title} img={thirthBook.img} />
+//     </section>
+//   )
+// }
+
+//********************************************* */
+
+const Book = (props) => {
+  return (
+    <article className="book">
+      <img src={props.img} />
+      <h2>{props.author}</h2>
+      <h4>{props.title}</h4>
+    </article>
+  )
+}
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirthBook.author}
+        title={thirthBook.title}
+        img={thirthBook.img}
+      />
+    </section>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
