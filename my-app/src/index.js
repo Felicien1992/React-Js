21507a10835c77f7f9599ff4bb5d991d40016396
Felicien1992 <felicienmukesharugamba@gmail.com>
2023-12-04 12:50:53 +0200

    I changed title & autho

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,43 +4,37 @@ import './index.css'
 
 
 const firstBook = {
-  img: './images/imag2.jpg',
-  author: 'Felicien',
-  title: 'Best Book',
+  img: './images/imag7.jpg',
+  author: 'Hannah Nicole Maehrer',
+  title: 'Assistant to the Villain',
 }
 const secondBook = {
   img: './images/imag5.jpg',
-  author: 'Blaisby',
-  title: 'Funny Book',
+  author: 'Rachel Howzell Hall',
+  title: 'The Last One',
 }
 const thirthBook = {
   img: './images/imag4.jpg',
-  author: 'Synforien',
-  title: 'Commedy Book',
+  author: 'Cecy Robson',
+  title: 'Bloodguard',
+}
+const fourthBook = {
+  img: './images/imag3.jpg',
+  author: 'Tracy Wolff and Nina Croft',
+  title: 'Star Bringer',
+}
+const fifthBook = {
+  img: './images/imag2.jpg',
+  author: 'Linde',
+  title: 'The Wren in the Holly Library',
 }
 
-// const Book = (props) => {
-//   return (
-//     <article className="book">
-//       <img src={props.img} />
-//       <h2>{props.author}</h2>
-//       <h4>{props.title}</h4>
-//     </article>
-//   )
-// }
+const sixthBook = {
+  img: './images/imag6.jpg',
+  author: 'Gakuba',
+  title: 'Sanctuary of the Shadow by Aurora Ascher',
+}
 
-
-// function BookList() {
-//   return (
-//     <section className="booklist">
-//       <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
-//       <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
-//       <Book author={thirthBook.author} title={thirthBook.title} img={thirthBook.img} />
-//     </section>
-//   )
-// }
-
-//********************************************* */
 
 const Book = (props) => {
   const {img,title,author} = props
@@ -71,9 +65,48 @@ function BookList() {
         title={thirthBook.title}
         img={thirthBook.img}
       />
+      <Book
+        author={fourthBook.author}
+        title={fourthBook.title}
+        img={fourthBook.img}
+      />
+      <Book
+        author={fifthBook.author}
+        title={fifthBook.title}
+        img={fifthBook.img}
+      />
+      <Book
+        author={sixthBook.author}
+        title={sixthBook.title}
+        img={sixthBook.img}
+      />
     </section>
   )
 }
+
+//********************************************* */
+
+
+// const Book = (props) => {
+//   return (
+//     <article className="book">
+//       <img src={props.img} />
+//       <h2>{props.author}</h2>
+//       <h4>{props.title}</h4>
+//     </article>
+//   )
+// }
+
+
+// function BookList() {
+//   return (
+//     <section className="booklist">
+//       <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
+//       <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+//       <Book author={thirthBook.author} title={thirthBook.title} img={thirthBook.img} />
+//     </section>
+//   )
+// }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
