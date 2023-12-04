3cde7235c71f881dcd36ae4818e1ab93828c8efc
Felicien1992 <felicienmukesharugamba@gmail.com>
2023-12-04 12:20:54 +0200

    add other method

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,17 +4,17 @@ import './index.css'
 
 
 const firstBook = {
-  img: './images/imag1.jpg',
+  img: './images/imag2.jpg',
   author: 'Felicien',
   title: 'Best Book',
 }
 const secondBook = {
-  img: './images/imag3.jpg',
+  img: './images/imag5.jpg',
   author: 'Blaisby',
   title: 'Funny Book',
 }
 const thirthBook = {
-  img: './images/imag6.jpg',
+  img: './images/imag4.jpg',
   author: 'Synforien',
   title: 'Commedy Book',
 }
@@ -43,11 +43,12 @@ const thirthBook = {
 //********************************************* */
 
 const Book = (props) => {
+  const {img,title,author} = props
   return (
     <article className="book">
-      <img src={props.img} />
-      <h2>{props.author}</h2>
-      <h4>{props.title}</h4>
+      <img src={img} />
+      <h2>{author}</h2>
+      <h4>{title}</h4>
     </article>
   )
 }
