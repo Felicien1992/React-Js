3a7300c9536fcbbf36181b7cebff5a6e4d7ee5b8
Felicien1992 <felicienmukesharugamba@gmail.com>
2023-12-05 13:28:17 +0200

    add new style css

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,142 +2,62 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const firstBook = {
-  img: './images/imag7.jpg',
-  author: 'Hannah Nicole Maehrer',
-  title: 'Assistant to the Villain',
-}
-const secondBook = {
-  img: './images/imag5.jpg',
-  author: 'Rachel Howzell Hall',
-  title: 'The Last One',
-}
-const thirthBook = {
-  img: './images/imag4.jpg',
-  author: 'Cecy Robson',
-  title: 'Bloodguard',
-}
-const fourthBook = {
-  img: './images/imag3.jpg',
-  author: 'Tracy Wolff and Nina Croft',
-  title: 'Star Bringer',
-}
-const fifthBook = {
-  img: './images/imag2.jpg',
-  author: 'Linde',
-  title: 'The Wren in the Holly Library',
-}
+const books = [
+  {
+    img: './images/imag7.jpg',
+    author: 'Hannah Nicole Maehrer',
+    title: 'Assistant to the Villain',
+  },
+  {
+    img: './images/imag5.jpg',
+    author: 'Rachel Howzell Hall',
+    title: 'The Last One',
+  },
+  {
+    img: './images/imag4.jpg',
+    author: 'Cecy Robson',
+    title: 'Bloodguard',
+  },
+  {
+    img: './images/imag3.jpg',
+    author: 'Tracy Wolff and Nina Croft',
+    title: 'Star Bringer',
+  },
+  {
+    img: './images/imag2.jpg',
+    author: 'Linde',
+    title: 'The Wren in the Holly Library',
+  },
 
-const sixthBook = {
-  img: './images/imag6.jpg',
-  author: 'Aurora Ascher',
-  title: 'Sanctuary of the Shadow',
-}
-
+  {
+    img: './images/imag6.jpg',
+    author: 'Aurora Ascher',
+    title: 'Sanctuary of the Shadow',
+  },
+]
 
 function BookList() {
   return (
-    <main>
-      <h1
-        style={{ 
-          color: '#457809',
-          fontFamily: 'serif', 
-          textAlign: 'center',
-          marginTop:"10px",
-          textDecoration:"underline"
-        }}
-      >
-        LIBRARY
-      </h1>
-      <section className="booklist">
-        <Book
-          author={firstBook.author}
-          title={firstBook.title}
-          img={firstBook.img}
-        >
-          <p className="p">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-            repudiandae inventore eos qui animi sed iusto alias eius ea
-            sapiente.
-          </p>
-          <button className="btn">By Now</button>
-        </Book>
-
-        <Book
-          author={secondBook.author}
-          title={secondBook.title}
-          img={secondBook.img}
-        >
-          <p className="p">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-            repudiandae inventore eos qui animi sed iusto alias eius ea
-            sapiente.
-          </p>
-          <button className="btn">By Now</button>
-        </Book>
-
-        <Book
-          author={thirthBook.author}
-          title={thirthBook.title}
-          img={thirthBook.img}
-        >
-          <p className="p">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-            repudiandae inventore eos qui animi sed iusto alias eius ea
-            sapiente.
-          </p>
-          <button className="btn">By Now</button>
-        </Book>
-
-        <Book
-          author={fourthBook.author}
-          title={fourthBook.title}
-          img={fourthBook.img}
-        >
-          <p className="p">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-            repudiandae inventore eos qui animi sed iusto alias eius ea
-            sapiente.
-          </p>
-          <button className="btn">By Now</button>
-        </Book>
-        <Book
-          author={fifthBook.author}
-          title={fifthBook.title}
-          img={fifthBook.img}
-        >
-          <p className="p">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-            repudiandae inventore eos qui animi sed iusto alias eius ea
-            sapiente.
-          </p>
-          <button className="btn">By Now</button>
-        </Book>
-        <Book
-          author={sixthBook.author}
-          title={sixthBook.title}
-          img={sixthBook.img}
-        >
-          <p className="p">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-            repudiandae inventore eos qui animi sed iusto alias eius ea
-            sapiente.
-          </p>
-          <button className="btn">By Now</button>
-        </Book>
-      </section>
-    </main>
+    <section className="booklist">
+      {books.map((book, id) => {
+        return <Book key={book.id} {...book}></Book>
+      })}
+    </section>
   )
 }
-
 const Book = (props) => {
-  const { img, title, author, children } = props
   return (
     <article className="book">
-      <img src={img} />
-      <h2>{author}</h2>
-      <h4>{title}</h4>
-      {children}
+      <img src={props.img} />
+      <h2>{props.title}</h2>
+      <h5>{props.author}</h5>
+      <p className='p'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum odio
+        enim accusantium vero, non nisi, dicta possimus expedita eos eligendi
+        vel itaque blanditiis, at omnis exercitationem consequuntur ipsam!
+        Harum, veniam!
+      </p>
+      <button className='btn'>Click Me</button>
     </article>
   )
 }
